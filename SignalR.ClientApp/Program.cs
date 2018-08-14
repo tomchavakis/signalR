d40ebd3198a3c599443d54a69eb9eeb204b1412d
tchav <t.chavakis@encodegroup.com>
2018-08-14 10:40:08 +0300

    Adding signalr Console Bearer Authentication

--- SignalR.ClientApp/Program.cs
+++ SignalR.ClientApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR.Client;
+﻿using Microsoft.AspNetCore.Http.Connections.Client;
+using Microsoft.AspNetCore.SignalR.Client;
 using System;
 using System.Threading.Tasks;
 
@@ -13,7 +14,16 @@ namespace SignalR.ClientApp
         {
             Console.WriteLine("SignalR Client Application Started");
 
-            connection = new HubConnectionBuilder().WithUrl("http://localhost:49897/integrationhub").Build();
+            var jwt = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYW5hbHlzdDIiLCJyb2xlcyI6IkFMRVYyIiwibmJmIjoxNTM0MjMwOTgzLCJleHAiOjE1MzQzMTczODMsImlzcyI6IlNvY3NUb2tlblNlcnZlciIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDQzNzYvIn0.6x3bhf9JsiwN8NjI5oREAmFyv8ptnvfs2HuTK2RxOSk";
+
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:49897/integrationhub?entity=case&entityId=376", options => {
+                    options.AccessTokenProvider = async () =>
+                    {
+                        return jwt;
+                    };
+                }).Build();
 
             connection.On<string>("ReceiveMessage", (message) =>
             {
@@ -40,6 +50,13 @@ namespace SignalR.ClientApp
                 Console.WriteLine(newMessage);
             });
 
+
+            connection.On<string>("onlineUsers", (message) =>
+            {
+                var newMessage = $"{message}";
+                Console.WriteLine(newMessage);
+            });
+
             try
             {
                 await TryConnectAsync();
@@ -49,7 +66,7 @@ namespace SignalR.ClientApp
                 Console.WriteLine(ex.ToString());
             }
 
-            await connection.InvokeAsync("SendMessage","thomas","thomas2");
+            await connection.InvokeAsync("SendMessage", "thomas", "thomas2");
 
 
             connection.Closed += Connection_Closed;
